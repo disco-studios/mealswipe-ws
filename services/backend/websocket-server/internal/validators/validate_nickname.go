ebba6f731a4b496575b00c2b1908c1e51d49f7b1
Cameron Lund <cameron@discostudios.io>
2021-10-24 15:31:48 -0400

    Allow many more nicknames

--- services/backend/websocket-server/internal/validators/validate_nickname.go
+++ services/backend/websocket-server/internal/validators/validate_nickname.go
@@ -1,13 +1,8 @@
 package validators
 
-import "regexp"
-
 func IsNicknameValid(nickname string) (valid bool, err error) {
 	if len(nickname) == 0 || len(nickname) > 16 {
 		return false, nil
 	}
-	// - Does not start or end with a space
-	// - Only contains a-zA-Z and space
-	// - Can only have one space in a row
-	return regexp.MatchString(`^([a-zA-Z]+ ?)*[a-zA-Z]$`, nickname)
+	return true, nil
 }
--- services/backend/websocket-server/internal/validators/validate_nickname_test.go
+++ services/backend/websocket-server/internal/validators/validate_nickname_test.go
@@ -12,41 +12,6 @@ import (
 func TestIsNicknameValid(t *testing.T) {
 	nickname := ""
 
-	t.Run("Lead space invalid", func(t *testing.T) {
-		nickname = " Cam the Man"
-		if valid, err := IsNicknameValid(nickname); valid && err == nil {
-			t.FailNow()
-		}
-	})
-
-	t.Run("Trail space invalid", func(t *testing.T) {
-		nickname = "Cam the Man "
-		if valid, err := IsNicknameValid(nickname); valid && err == nil {
-			t.FailNow()
-		}
-	})
-
-	t.Run("NonAlphanumeric Invalid", func(t *testing.T) {
-		nickname = "C@m the Man "
-		if valid, err := IsNicknameValid(nickname); valid && err == nil {
-			t.FailNow()
-		}
-	})
-
-	t.Run("More than 2 spaces in a row invalid", func(t *testing.T) {
-		nickname = "Cam  the Man "
-		if valid, err := IsNicknameValid(nickname); valid && err == nil {
-			t.FailNow()
-		}
-	})
-
-	t.Run("Using other whitespace invalid", func(t *testing.T) {
-		nickname = "Cam\tthe Man "
-		if valid, err := IsNicknameValid(nickname); valid && err == nil {
-			t.FailNow()
-		}
-	})
-
 	t.Run("Too long invalid", func(t *testing.T) {
 		nickname = "Cam the Mannnnnnnnnnnn"
 		if valid, err := IsNicknameValid(nickname); valid && err == nil {
